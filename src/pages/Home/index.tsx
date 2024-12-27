36a9bc96da175ba144b9ac13d88c6da153fc260f
Helio Leao <hmtleao@hotmail.com>
2024-12-27 18:41:44 -0300

    feat: fetch and display user's pets on Home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,13 +2,27 @@ import { Link } from "react-router-dom";
 import styles from "./style.module.css";
 import { useSession } from "../../contexts/session";
 import { Navigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import api from "../../api/api";
 
 export default function Home() {
   const { session, signOut } = useSession();
+  const [pets, setPets] = useState<any>([]);
 
   if (!session) {
     return <Navigate to="/login" />;
   }
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const { data } = await api.get(`/users/${session._id}/pets`);
+        setPets(data);
+      } catch (error) {
+        console.error(error);
+      }
+    })();
+  }, []);
 
   function handleLogout() {
     signOut();
@@ -25,6 +39,17 @@ export default function Home() {
       </div>
       <div className={styles.linksContainer}>
         <Link to="/new-pet">Add Pet</Link>
+      </div>
+
+      <div style={{ marginTop: 20 }}>
+        {pets.map((pet: any) => (
+          <div key={pet._id}>
+            <p>{pet.name}</p>
+            <p>{pet.type}</p>
+            <p>{pet.breed}</p>
+            <p>{pet.birthdate}</p>
+          </div>
+        ))}
       </div>
     </main>
   );

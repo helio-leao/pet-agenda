669d222d1b1d3f6420ea9d36e41d9e5015d28742
Helio Leao <hmtleao@hotmail.com>
2025-01-10 10:56:17 -0300

    refactor: simplify layout structure in HomePage component by adjusting class order

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -53,7 +53,7 @@ export default function HomePage() {
         </div>
       </div>
 
-      <div className="flex gap-2 justify-self-end mb-4">
+      <div className="flex justify-end gap-2 mb-4">
         <Link
           to="/pets/new"
           className="bg-sky-600 rounded-lg px-4 py-2 self-start text-white"
